351f8e49e7d492b8223c2a5eb8edaabdbf5bc7a0
chan1055 <142316049+chan1055@users.noreply.github.com>
2024-03-22 19:51:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,63 @@
 from flask import Flask, request, render_template
+ 
+from flask import Flask, request, render_template
 import datetime
 import sqlite3
 from flask import Markup
-
+ 
 app = Flask(__name__)
-
+ 
 name_flag = 0
-name = ""
-
+name=""
+ 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
-
+ 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    global name_flag, name
-    if name_flag ==0:        
+    global name_flag,name
+    if name_flag==0:
         name = request.form.get("name")
-        name_flag = 1
+        name_flag=1
         conn = sqlite3.connect("log.db")
         c = conn.cursor()
         timestamp = datetime.datetime.now()
-        c.execute("insert into employee (name,timestamp) values(?,?)", (name, timestamp))
+        c.execute("insert into employee (name,timestamp) values(?,?)",(name,timestamp))
         conn.commit()
         c.close()
         conn.close()
     return(render_template("main.html",name=name))
-
+ 
 @app.route("/ethical_test",methods=["GET","POST"])
 def ethical_test():   
     return(render_template("ethical_test.html"))
-
+ 
 @app.route("/query",methods=["GET","POST"])
-def query():   
+def query():
     conn = sqlite3.connect("log.db")
     c = conn.execute("select * from employee")
-    r = ""
+    r=""
     for row in c:
-        r = r+str(row)+"<br>"
+        r=r+str(row)+"<br>"
     print(r)
     r = Markup(r)
     c.close()
     conn.close()
     return(render_template("query.html",r=r))
 
+@app.route("/delete",methods=["GET","POST"])
+def delete():
+    conn = sqlite3.connect("log.db")
+    c = conn.cursor()
+    c.execute("delete from employee")
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("delete.html"))
+ 
 @app.route("/answer",methods=["GET","POST"])
-def answer():   
+def answer():
     ans = request.form["options"]
     print(ans)
     if ans == "true":
@@ -53,8 +65,20 @@ def answer():
     else:
         return(render_template("correct.html"))
 
+@app.route("/food_exp",methods=["GET","POST"])
+def food_exp():
+    return(render_template("food_exp.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():   
+    income=float(request.form.get("income"))
+    return(render_template("prediction.html", r=(income * 0.485)+147))    
+
+
+ 
 @app.route("/end",methods=["GET","POST"])
 def end():  
     return(render_template("end.html"))
+ 
 if __name__ == "__main__":
     app.run()
